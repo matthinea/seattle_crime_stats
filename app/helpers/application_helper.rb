bfcf0b0b03e1e6b203e87144ce3e573ad5e9e9a3
“Matthew <matthew.hinea@gmail.com>
2016-09-03 15:54:58 -0700

    add uptick/downtick colum

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def compare(period, overall)
+    return 0 if period == 0
+    diff = overall - period
+    diff / period * 100
+  end
+
+  def months_in_dataset
+    72.0
+  end
+
 end
