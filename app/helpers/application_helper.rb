277277987aa4617f02ad91d798a9f5a028258155
“Matthew <matthew.hinea@gmail.com>
2016-09-03 16:46:02 -0700

    finish uptick downtick, stye flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
     72.0
   end
 
+  def arrow_image_tag(tick)
+    if tick > 0
+     image_tag("red_up_arrow.svg.png", size: "16x10")
+    else
+     image_tag("green_down_arrow.svg.png", size: "16x10")
+   end
+ end
+
 end
